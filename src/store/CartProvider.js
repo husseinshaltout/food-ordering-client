949bbd50d10175f3f921ee629e0dd4e2ce1f51f0
Hussein Shaltout <48390004+husseinshaltout@users.noreply.github.com>
2023-01-25 14:36:29 +0000

    feat(cart): Add Add Item & Remove Item Logiic to Cart Provider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -9,25 +9,70 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
 	if (action.type === "ADD") {
-		const updateItems = state.items.concat(action.item);
 		const updatedTotalAmount =
 			state.totalAmount + action.item.price * action.item.amount;
-		return { items: updateItems, totalAmount: updatedTotalAmount };
+
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.item.id
+		);
+
+		const existingCartItem = state.items[existingCartItemIndex];
+
+		let updatedItems;
+
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount + action.item.amount,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			updatedItems = state.items.concat(action.item);
+		}
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+
+	if (action.type === "REMOVE") {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+
+		const existingCartItem = state.items[existingCartItemIndex];
+
+		const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+		let updatedItems;
+
+		if (existingCartItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id !== action.id);
+		} else {
+			const updatedItem = {
+				...existingCartItem,
+				amount: existingCartItem.amount - 1,
+			};
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
+		}
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
 	}
 	return defaultCartState;
 };
 
 const CartProvider = (props) => {
-	const [carState, dispatchCartAction] = useReducer(
+	const [cartState, dispatchCartAction] = useReducer(
 		cartReducer,
 		defaultCartState
 	);
 
 	const addItemToCartHandler = (item) => {
-		dispatchCartAction({
-			type: "ADD",
-			item: item,
-		});
+		dispatchCartAction({ type: "ADD", item: item });
 	};
 
 	const removeItemFromCartHandler = (id) => {
@@ -35,8 +80,8 @@ const CartProvider = (props) => {
 	};
 
 	const cartContext = {
-		items: carState.items,
-		totalAmount: 0,
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
 	};
