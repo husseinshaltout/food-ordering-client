93507193fbe4366294d7a732e7d7911709544624
Hussein Shaltout <48390004+husseinshaltout@users.noreply.github.com>
2023-01-26 08:25:54 +0000

    feat(header): Update Header Style

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -3,14 +3,11 @@ import classes from "./MealsSummary.module.css";
 const MealsSummary = () => {
 	return (
 		<section className={classes.summary}>
-			<h2>Delicious Food, Delivered To You</h2>
+			<h2>Eden Solutions Office Ordering</h2>
 			<p>
-				Choose your favorite meal from our broad selection of available
-				meals and enjoy a delicious lunch or dinner at home.
-			</p>
-			<p>
-				All our meals are cooked with high-quality ingredients,
-				just-in-time and of course by experienced chefs!
+				Choose your favorite product from our broad selection of
+				available food and beverages and enjoy a delicious snack or
+				drink at our office.
 			</p>
 		</section>
 	);
