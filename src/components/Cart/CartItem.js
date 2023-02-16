fdf1ed3327f652078bcaa9657435aca1cda6acab
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2023-02-16 16:31:41 +0200

    Add Conditional Extras Label

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -2,17 +2,18 @@ import React from "react";
 import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
+	const hasOptions = props.item.options.length > 0;
 	return (
 		<li className={classes["cart-item"]}>
 			<div>
 				<h2>{props.item.name}</h2>
 				<div className={classes.summary}>
+					{hasOptions && "Extras:"}
 					{props.item.options.map((option) => (
 						<span key={option._id} className={classes.price}>
 							{option.name}
 						</span>
 					))}
-
 					<span className={classes.amount}>x {props.qty}</span>
 				</div>
 			</div>
